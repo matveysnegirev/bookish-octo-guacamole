ca8b83ec886f37426f7875705a95a3cace0f185d
matveysnegirev <matveysnegirev@mail.ru>
2025-08-08 18:56:07 +0600

    Add files via upload

---
+++ DZ LESON2.py
@@ -0,0 +1,16 @@
+list=[100,200,300]
+x=list[1]-list[2]
+print(x)
+tuple = (1,2,3,4.2)
+print(tuple)
+
+x=2+list[0]
+dict1 = {
+    1: 67,
+    2: 'hello world',
+    3: 'hello',
+    4: 67,
+    5: 90,
+    6: 57,
+}
+print('Вывод',dict1[5])
